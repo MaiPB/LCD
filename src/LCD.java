ec0657c1aa2a3d98fb5274c13872c6eac4c84cc1
MaiPB <103893595+MaiPB@users.noreply.github.com>
2022-04-18 00:00:48 +0700

    Initial commit

---
+++ src/LCD.java
@@ -0,0 +1,13 @@
+public class LCD {
+    public static void main(String[] args) {
+LCDINFO.sayClearScreen();
+LCDINFO.sayShowMainMenu();
+LCDINFO.sayLoginMenu();
+LCDINFO.sayShowMainMenu();
+LCDINFO.sayChangeACSetP();
+LCDINFO.sayConfirmRestart();
+LCDINFO.sayConfirmShutdown();
+LCDINFO.sayRestart();
+LCDINFO.sayShowShutdown();
+    }
+}
---
+++ src/LCDINFO.java
@@ -0,0 +1,30 @@
+public class LCDINFO {
+    public static void sayClearScreen(){
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
